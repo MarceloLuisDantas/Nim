06d591dae7a1eaf3873ef135c09af064012a8f65
Marcelo Luis <marceloluis257gh@gmail.com>
2023-04-12 21:45:41 -0300

    coisas

--- Guia/4 Coleções/main.nim
+++ Guia/4 Coleções/main.nim
@@ -1,4 +1,4 @@
-# Arrays
+# # Arrays
 
 # var lista: array[4, int] = [1, 2, 3, 4]  ✔️
 
@@ -9,11 +9,49 @@
 # var tamanho = input("Qual o tamanho?")
 # var lista: array[tamanho, int]
 
-# Sequencias 
+# # Sequencias 
+
+# var sequencia: seq[int]
+# var iniciais: seq[string] = @["Meowscarada", "Quaquaval", "Skeledirge"]
+# var time = newSeq[string](6)
+# var ubers = newSeqOfCap[string](10)
+
+# import std/sequtils
+# ubers = concat(ubers, @["Miraidon", "Koraidon", "Flutter Mane", "Annihilape"])
 
 
+# var sequencia = newSeq[int](6)
+# # resulta em @[0, 0, 0, 0, 0, 0]
+# var sequencia = newSeqOfCap[int](6)
+# # resulta em @[]
 
-# Array 2 Seq
+# # Array 2 Seq
 
-# Slice
+# var lista: array[5, int] = [1, 2, 3, 4, 5]
+# var sequencia: seq[int] = @lista 
+
+# var lista2 = seqToArray(sequencia)
+
+# # Slice
+
+# var lista = [1, 2, 3, 4, 5]
+# var sequencia = @[9, 8, 7, 6, 5]
+
+# echo sequencia[2..4]  # @[7, 6, 5]
+# echo lista[3]         # 4
+
+
+# sequencia[2..4] = [1]          ✔️
+# sequencia[2..4] = [1, 2, 3, 4] ✔️
+
+# lista[1..3] = [4, 2, 5]  ✔️
+# lista[1..3] = [4, 2]     ❌
+
+# # Tuplas
+
+# var move = (nome: "Gigaton Hammer", poder: 160)
+# # var move = ("Gigaton Hammer", 160)
+# echo move.nome  # retorna 'Gigaton Hammer'
+# echo move[1]    # retorna 160
+
 
--- README.md
+++ README.md
@@ -3,11 +3,17 @@ Estudos Nim
 
 ## Guia 
 [Tipos Primitivos](Guia/1%20Tipos%20Primitivos/main.nim)
+
 [Input Output](Guia/2%20Input%20Output/main.nim)
+
 [Controle de Fluxo](Guia/3%20Controle%20de%20Fluxo/main.nim)
+
 [Coleções](Guia/4%20Cole%C3%A7%C3%B5es/main.nim)
+
 [Proc e Func](Guia/5%20Proc%20e%20Func/main.nim)
+
 [Outras Coleções](Guia/6%20Outras%20Cole%C3%A7%C3%B5es/main.nim)
+
 [Parametros Mutaveis](Guia/7%20Parametros%20Mutaveis/main.nim)
 
 ## Estruturas
